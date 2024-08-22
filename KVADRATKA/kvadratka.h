737a535323f456458a11c009e1a6096b0d31424f
71frukt <71frukt@mail.ru>
2024-08-22 16:03:12 +0300

    com 9
    добавлен юнитест + общий рефакторинг

--- KVADRATKA/kvadratka.h
+++ KVADRATKA/kvadratka.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 enum count_of_roots
 {
     NO_ROOTS,
@@ -6,16 +8,61 @@ enum count_of_roots
     INFIN_ROOTS,
     ERROR_ROOTS
 };
-count_of_roots general_solve_equation(float a, float b, float c, float *root_1, float *root_2);     //решает введённое уравнение и возвращает количество корней
-count_of_roots solve_linear_equation(float b, float c, float *root_1);                              //решает уравнение при a == 0 (линейное или если все коэффициенты == 0)
-count_of_roots solve_quadratic_equation(float a, float b, float c, float *root1, float *root2);     //решает квадратное уравнение и возвращает количество корней
+count_of_roots GeneralSolveEquation(const float a, const  float b, const float c, float *root_1, float *root_2);     //решает введённое уравнение и возвращает количество корней
+count_of_roots SolveLinearEquation(const float b, const float c, float *root_1);                              //решает уравнение при a == 0 (линейное или если все коэффициенты == 0)
+count_of_roots SolveQuadraticEquation(const float a, const float b, const float c, float *root1, float *root2);     //решает квадратное уравнение и возвращает количество корней
+
+enum menu_code
+{
+    MENU_ERROR = -1,
+    MENU_EXIT = 0,
+    MENU_CONTINUE = 1
+};
+menu_code ExitMenu();  
+
+struct test_values
+{
+    float a, b, c;
+    
+    float x1_exp, x2_exp;
+    count_of_roots n_roots_exp;
+};
+
 
 bool GetInput(float *a, float *b, float *c);                                                        //получает ввод
 bool GetValue(float *a, float *b, float *c);                                                        //true, если принял все значения и false, если не все
 void PrintSolve(count_of_roots n_roots, float root_1, float root_2);                                           //выводит решение 
-int ExitMenu();                                                                                     //позволяет выбрать выйти из программы или остаться в ней
 bool CleanBuffer();
 bool IsDifferentF(float num_1, float num_2);
 bool IsZeroF(float num);
 
+void UnitTest(test_values tests[], int num_tests);
+void RunTest(test_values test, int num_test);
+const char *GetCountOfRoots(count_of_roots n_roots);
 const float LOWCONST = 0.0001;
+const int ENUM_STR_LEN = 30;
+
+struct test_values tests[] =
+{ //  a,    b,   c,  x1_exp,  x2_exp,  n_roots_exp
+ 
+    // int  values
+    { 0,    0,   0,    NAN,     NAN,    INFIN_ROOTS },    // a == b == c == 0
+    { 0,    0,   1,    NAN,     NAN,    NO_ROOTS },       // a == b == 0, c != 0
+    { 0,    1,   1,    -1,      NAN,    ONE_ROOT },       // a == 0, b != 0, c != 0
+  
+    { 1,    1,   1,    NAN,     NAN,    NO_ROOTS },       // discr < 0
+    { 1,   -4,   4,    2,       NAN,    ONE_ROOT },       // discr == 0
+    { 1,   -1,  -2,    -1,      2,      TWO_ROOTS },      // discr > 0
+  
+
+    // floa t values 
+    { 0.0,  0.0, 0.0,  NAN,     NAN,    INFIN_ROOTS },    // a == b == c == 0
+    { 0.0,  0.0, 1.0,  NAN,     NAN,    NO_ROOTS },       // a == b == 0, c != 0
+    { 0.0,  1.0, 1.0, -1.0,     NAN,    ONE_ROOT },       // a == 0, b != 0, c != 0
+  
+    { 1.0,  1.0, 1.0,  NAN,     NAN,    NO_ROOTS },       // discr < 0
+    { 1.0, -4.0, 4.0,  2.0,     NAN,    ONE_ROOT },       // discr == 0
+    { 1.0, -1.0,-2.0, -1.0,    2.0,     TWO_ROOTS }       // discr > 0
+};
+
+
--- KVADRATKA/main.cpp
+++ KVADRATKA/main.cpp
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <ctype.h>
+#include <string.h>
 #include "kvadratka.h"
-
 
 
 int main()
@@ -14,7 +14,10 @@ int main()
     float b = NAN;
     float root_1 = NAN;
     float root_2 = NAN;
-    
+
+    UnitTest(tests, 12);
+
+    /*
     printf("\nThis program solves a quadratic equation. \n");
 
     while (true)
@@ -22,13 +25,18 @@ int main()
         if (GetInput(&a, &b, &c) == false)
             continue;
         
-        count_of_roots n_roots = general_solve_equation(a, b, c, &root_1, &root_2);
+        count_of_roots n_roots = GeneralSolveEquation(a, b, c, &root_1, &root_2);
         PrintSolve(n_roots, root_1, root_2);
 
-        if (ExitMenu() == 0)
+        menu_code code = ExitMenu();
+        if (code == MENU_EXIT)
             break;
+        else if (code == MENU_CONTINUE)
+            continue;
+        else
+            main_exit_code = MAIN_ERROR;
     }
-
+    */
     printf("The program is completed. \n");
     return 0;
 }
@@ -37,11 +45,10 @@ bool GetInput(float *a, float *b, float *c)
 {
     printf("\nEnter three coefficients (a b c) for the solution.\n");
 
-    if(GetValue(a, b, c) == false){
+    if (GetValue(a, b, c) == false) {
         printf("Incorrect input! Try again. \n");
         return false;
     }
-
     return true; 
 }
 
@@ -71,40 +78,39 @@ bool CleanBuffer()
 
     while ((curr_ch = getchar()) != '\n')       //пропускаем остальной ввод, если он был
     {       
-        if(!isspace(curr_ch)){
+        if (!isspace(curr_ch)) 
             success_clean = false;
-        }    
     } 
 
     return success_clean;
 }
 
-count_of_roots general_solve_equation(float a, float b, float c, float *root_1, float *root_2)
+count_of_roots GeneralSolveEquation(const float a, const float b, const float c, float *root_1, float *root_2)
 {
     assert(root_1);
     assert(root_2);
     assert(root_1 != root_2);
 
-    count_of_roots n_roots = NO_ROOTS;
+    count_of_roots n_roots = ERROR_ROOTS;
     if (IsZeroF(a)) {                                  //if (a == 0)
-        n_roots = solve_linear_equation(b, c, root_1);
+        n_roots = SolveLinearEquation(b, c, root_1);
         *root_2 = NAN;
     }
     else    //введённое квадратное уравнение всё-таки действительно квадратное   
-        n_roots = solve_quadratic_equation(a, b, c, root_1, root_2);    //количество корней
+        n_roots = SolveQuadraticEquation(a, b, c, root_1, root_2);    //количество корней
     
     return n_roots;
 }
 
-count_of_roots solve_linear_equation(float b, float c, float *root_1)
+count_of_roots SolveLinearEquation(const float b, const float c, float *root_1)
 {
     assert(root_1);
 
-    if (IsZeroF(b) && IsZeroF(c))             //if (b == 0 && c == 0)
+    if (IsZeroF(b) && IsZeroF(c))               //if (b == 0 && c == 0)
         return INFIN_ROOTS;
     else if (IsZeroF(b))                        //else if (b == 0 && c != 0)
         return NO_ROOTS;        
-    else {                                        //else if (b != 0 && c != 0)
+    else {                                      //else if (b != 0 && c != 0)
         *root_1 = - c / b;
         return ONE_ROOT;        
     }   
@@ -112,29 +118,29 @@ count_of_roots solve_linear_equation(float b, float c, float *root_1)
     return ERROR_ROOTS;
 }
 
-count_of_roots solve_quadratic_equation(float a, float b, float c, float *root_1, float *root_2)
+count_of_roots SolveQuadraticEquation(const float a, const float b, const float c, float *root_1, float *root_2)
 {
     assert(root_1);
     assert(root_2);
     assert(root_1 != root_2);
 
-    float Discr = b * b - 4 * a * c; 
+    float discr = b * b - 4 * a * c; 
 
-    if (Discr < 0)
+    if (discr < 0)
         return NO_ROOTS;
 
-    else if (IsZeroF(Discr)) {        //if D == 0
+    else if (IsZeroF(discr)) {        //if D == 0
         *root_1 = - b / (2 * a);
-        *root_2 = *root_1;
+        *root_2 = NAN;
 
         return ONE_ROOT; 
     }
 
     else {
-        float sqrt_Discr = sqrt(Discr);
+        float sqrt_discr = sqrt(discr);
 
-        *root_1 = (- b - sqrt_Discr) / (2 * a);
-        *root_2 = (- b + sqrt_Discr) / (2 * a);
+        *root_1 = (- b - sqrt_discr) / (2 * a);
+        *root_2 = (- b + sqrt_discr) / (2 * a);
 
         assert(IsDifferentF(*root_1, *root_2));
 
@@ -144,7 +150,7 @@ count_of_roots solve_quadratic_equation(float a, float b, float c, float *root_1
     return ERROR_ROOTS;
 }
 
-void PrintSolve(count_of_roots n_roots, float root_1, float root_2)
+void PrintSolve(count_of_roots n_roots, const float root_1, const float root_2)
 {
     switch (n_roots)
     {
@@ -167,7 +173,7 @@ void PrintSolve(count_of_roots n_roots, float root_1, float root_2)
     }
 }
 
-int ExitMenu()
+menu_code ExitMenu()
 {
     char enter_ch = 0;
 
@@ -176,24 +182,80 @@ int ExitMenu()
         printf("\nEnter \'y\' if you want to continue or \'n\' to exit: ");
         scanf("%c", &enter_ch);
 
-
-        if(CleanBuffer() != true){         //если далее идут символы
+        if (CleanBuffer() == false) {         //если далее идут символы
             printf("Incorrect input.");
             continue;
         }
 
-        if ( tolower(enter_ch) == 'y' )
-            return 1;
-        else if ( tolower(enter_ch) == 'n' )
-            return 0;
+        if (tolower(enter_ch) == 'y')
+            return MENU_CONTINUE;
+        else if (tolower(enter_ch) == 'n')
+            return MENU_EXIT;
         else
             printf ("Incorrect input.");    //если enter_ch не y и не n  
     }
+
+    printf("Abnormai ExitMenu code");
+    return MENU_ERROR;
 }
 
+void UnitTest(test_values tests[], int num_tests)
+{
+    for (int i = 0; i < num_tests; i++) 
+        RunTest(tests[i], i);
+}
+
+//void RunTest(int num_test, float a, float b, float c, float x1_exp, float x2_exp, count_of_roots n_roots_exp)
+void RunTest(test_values test, int num_test)
+{
+    float x1 = NAN;
+    float x2 = NAN;
+
+    count_of_roots n_roots = GeneralSolveEquation(test.a, test.b, test.c, &x1, &x2);
+
+    if (IsDifferentF(n_roots, test.n_roots_exp) || IsDifferentF(x1, test.x1_exp) || IsDifferentF(x2, test.x2_exp)) 
+    {
+        printf("Error test %d: a = %g, b = %g, c = %g, x1 = %g, x2 = %g, n_roots = %s \n"
+               "Expected: x1 = %g, x2 = %g, n_roots = %s \n\n",
+                (num_test + 1), test.a, test.b, test.c, x1, x2, GetCountOfRoots(n_roots), test.x1_exp, test.x2_exp, GetCountOfRoots(test.n_roots_exp));
+    }
+    else 
+        printf("COMPLETED test %d \n", num_test + 1);
+}
+
+const char *GetCountOfRoots(count_of_roots n_roots)
+{
+    switch (n_roots)
+    {
+        case NO_ROOTS:
+            return "NO_ROOTS";
+            break;
+        case ONE_ROOT:
+            return "ONE_ROOT";
+            break;
+        case TWO_ROOTS:
+            return "TWO_ROOTS";
+            break;
+        case INFIN_ROOTS:
+            return "INFIN_ROOTS";
+            break;
+        default:
+            printf("Error of running test! Count of roots is incorrect \n");
+            break;
+    }
+
+    return "\0";
+}
 
 bool IsDifferentF(float num_1, float num_2)
 {
+    if (isnan(num_1) || isnan(num_2)) {
+        if (isnan(num_1) && isnan(num_2))
+            return false;
+        else
+            return true;
+    }
+
     return (fabs(num_1 - num_2) > LOWCONST);
 }
 
